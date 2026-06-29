88db3278cc3c5eb20ddb04be05b1cf5c81f6dd99
Joeseph-M <mahesvaranjoseph@gmail.com>
2026-06-29 19:45:49 +0800

    final edit shifting to new project

--- app.py
+++ app.py
@@ -3,20 +3,37 @@ from config_db import get_db_connection
 
 app = Flask(__name__)
 
-def load_jobs(id):
+
+def load_jobs():
     db = get_db_connection()
     cursor = db.cursor(dictionary=True)
-    cursor.execute("SELECT * FROM jobs WHERE id = %s", (id,))
-    jobs = cursor.fetchone()
 
+    cursor.execute("SELECT * FROM jobs")
+    jobs = cursor.fetchall()
     cursor.close()
     db.close()
 
     return jobs
 
+
+def load_job(id):
+    db = get_db_connection()
+    cursor = db.cursor(dictionary=True)
+
+    cursor.execute(
+        "SELECT * FROM jobs WHERE id = %s",(id,))
+    job = cursor.fetchone()
+
+    cursor.close()
+    db.close()
+
+    return job
+
+
 @app.route("/")
 def home():
     jobs = load_jobs()
+
     return render_template(
         "home.html",
         jobs=jobs,
@@ -25,11 +42,11 @@ def home():
 
 @app.route("/job/<id>")
 def show_job(id):
-    job = load_jobs(id)
+    job = load_job(id)
     return render_template(
-        'jobpage.html',
+        "jobpage.html",
         job=job
     )
 
-if __name__=="__main__":
+if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
